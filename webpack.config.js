2627a9a5cdf4b49949bb3a1a231a0df67dc21d49
Heejong Ahn <heejongahn@gmail.com>
2017-05-24 18:44:24 +0900

    Add typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/main.ts',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
@@ -14,15 +14,21 @@ module.exports = {
         test: /\.vue$/,
         loader: 'vue-loader',
         options: {
-          loaders: {
-          }
-          // other vue-loader options go here
+          esModule: true,
         }
       },
       {
         test: /\.js$/,
         loader: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/,
+        options: {
+          appendTsSuffixTo: [/\.vue$/]
+        }
       },
       {
         test: /\.(png|jpg|gif|svg)$/,
@@ -34,6 +40,7 @@ module.exports = {
     ]
   },
   resolve: {
+    extensions: ['.ts', '.js'],
     alias: {
       'vue$': 'vue/dist/vue.esm.js'
     }
