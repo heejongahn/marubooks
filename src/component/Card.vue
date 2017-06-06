b1c6093ea3942bc2aa223935d7894d9e883269fa
Heejong Ahn <heejongahn@gmail.com>
2017-06-06 16:15:59 +0900

    Revise Card component style

--- src/component/Card.vue
+++ src/component/Card.vue
@@ -1,6 +1,14 @@
 <template>
   <div :class="$style.card">
-    <img :src="book.cover_s_url" />
+    <img :class="$style.bookCover" :src="book.cover_s_url" />
+    <div :class="$style.bookMeta">
+      <div :class="$style.title">
+        {{ book.title }}
+      </div>
+      <div :class="$style.publisher">
+        {{ book.author }}
+      </div>
+    </div>
     <!--
     <div>{{ book.title }}</div>
     <div>{{ book.pub_nm }}</div>
@@ -31,16 +39,43 @@ export default class Card extends Vue {
 
 <style module>
 .card {
-  flex: 1 1 0;
+  display: flex;
+
+  flex: 0 1 0;
   padding: 12px;
   margin: 8px;
 
-  border-radius: 4px;
-  border: 1px solid #ced3d6;
+  border-radius: 12px;
+
+  background: #FFFFFF;
+  min-width: 320px;
 }
 
 .card:hover {
-  background-color: #ced3d6;
+  background-color: rgba(0, 0, 0, 0.05);
   color: white;
+}
+
+.book-cover {
+  height: 100%;
+  margin-right: 24px;
+}
+
+.book-meta {
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+}
+
+.title {
+  word-break: keep-all;
+  font-size: 14px;
+  font-weight: bold;
+  margin-bottom: 4px;
+}
+
+.publisher {
+  font-size: 12px;
+  color: rgb(12, 12, 12);
 }
 </style>
